47a92ca31ab24150a77747a6087025a3c4a1190c
arbabnasru <148573131+arbabnasrullahkhan@users.noreply.github.com>
2026-06-15 00:32:01 -0700

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+// firebase-config.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc, collection, query, where, getDocs, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3DCiKSeFMpUAmz_iiuhnH-N3SgV-pIXU",
+    authDomain: "nesports7.firebaseapp.com",
+    projectId: "nesports7",
+    storageBucket: "nesports7.firebasestorage.app",
+    messagingSenderId: "414629589331",
+    appId: "1:414629589331:web:2a30069688008d17d371a6",
+    measurementId: "G-ELZMHDM7NM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Exporting to use in all other HTML files
+export { app, auth, db, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, doc, getDoc, setDoc, updateDoc, collection, query, where, getDocs, orderBy, limit };
